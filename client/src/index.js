8aaeac0ab8a6bb09b36f4ccfe69daf82371ca27d
mbalsamo <github email address>
2018-05-23 16:46:51 -0400

    move provider and store into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
