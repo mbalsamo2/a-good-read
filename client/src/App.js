eca7943a52c6d25464ada7f4fa746ba273aac425
mbalsamo <github email address>
2018-05-23 15:22:59 -0400

    create reducers, actions, and components directories and index file to hold reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { rootReducer } from './reducers/index'
 import logo from './logo.svg';
 import './App.css';
 
 const store = createStore(
-  reducerFunction,
+  rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ &&
   window.__REDUX_DEVTOOLS_EXTENSION__()
 );
