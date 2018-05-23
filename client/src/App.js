3c5876c527436cac065418e2f23737b60dfcae3f
mbalsamo <github email address>
2018-05-23 15:13:42 -0400

    add react and redux and update app contents

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import logo from './logo.svg';
 import './App.css';
+
+const store = createStore(
+  reducerFunction,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 class App extends Component {
   render() {
     return (
+    <Provider>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hold my place.
       </div>
+    </Provider>
     );
   }
 }
