2dd625b6cb64bb4a63de0522800aa18006b3ca98
mbalsamo <github email address>
2018-05-24 15:32:59 -0400

    create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524191803) do
+ActiveRecord::Schema.define(version: 20180524192929) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20180524191803) do
     t.datetime "updated_at", null: false
     t.text "review"
     t.integer "rating"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
