dec1e9cd78d77fcb8e156ee70500b6641284fe17
mbalsamo <github email address>
2018-05-31 15:08:08 -0400

    add book.css file and add some simple styling to make all the book images the same size

--- client/src/components/book.js
+++ client/src/components/book.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import  './book.css';
 
 const Book = ({ book }) => {
   return (
-    <div>
+    <div className="bookDiv">
       <Link to={`/books/${book.id}`}>
-        <img src={`${book.image_url}`} alt={book.name} />
+        <img className="bookImage" src={`${book.image_url}`} alt={book.name} />
       </Link>
       <p>Title: {book.name} || Author: {book.author}</p>
     </div>
