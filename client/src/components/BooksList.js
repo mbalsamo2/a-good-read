06050aa0cdd5c41fcb413c08e1ae1d5b6621b1bf
mbalsamo <github email address>
2018-05-31 13:36:20 -0400

    abstract away individual book presentation into book.js component

--- client/src/components/BooksList.js
+++ client/src/components/BooksList.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import '../components/NavBar.css';
 import { Link } from 'react-router-dom';
-// import BooksList from './containers/BooksList.js';
+import Book from './book.js';
 
 class BooksList extends Component {
 
   render() {
     if (this.props.books) {
       const books = this.props.books.map((book, index) => {
-        return <p>{book.name}</p>
+        return (
+        <Book book={book} /> )
       })
 
       return (
---
+++ client/src/components/book.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Book = ({ book }) => {
+  return (
+    <p>{book.author}</p>
+  )
+}
+
+export default Book;
