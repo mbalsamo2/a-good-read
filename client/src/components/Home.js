7fc54db08143aa8d3a5bcbccf40935c9bcb2ba7a
mbalsamo <github email address>
2018-06-01 09:54:37 -0400

    add styling to homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,7 @@ import './NavBar.css'
 
 export const Home = () =>
     <div className="HomePage">
-      <h1>A Good Read</h1>
-      <p>Add, review, and comment on popular book choices.</p>
+      <img src="https://windermerehoodcanal.com/files/2018/03/content_content_Bookshelf_01.jpg" />
+      <h1 className="appName">A Good Read</h1>
+      <p className="description">A place to add, review, and comment on popular book choices.</p>
     </div>
