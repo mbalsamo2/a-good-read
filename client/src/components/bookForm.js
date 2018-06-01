17932a9d1293392859c0f0c9e8b2a4fd23403ee2
mbalsamo <github email address>
2018-06-01 11:21:07 -0400

    build out BookForm form

--- client/src/components/bookForm.js
+++ client/src/components/bookForm.js
@@ -20,7 +20,14 @@ class BookForm extends Component {
   render() {
     return (
       <div>
-        <form>
+        <form id="book-form">
+          Title: <input type="text" name="name"/><br>
+          Author: <input type="text" name="author"/><br>
+          Summary: <textarea name="summary"/><br>
+          Review: <textarea name="review"/><br>
+          Rating: <input type="number" name="rating"/><br>
+          Book Image: <input type="text" name="image_url"/><br>
+          <input type="submit" />
         </form>
       </div>
     )
