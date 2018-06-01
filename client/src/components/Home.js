3edfbfe8bb45fc124b47ea557cebfc2aad28c1ab
mbalsamo <github email address>
2018-06-01 10:00:45 -0400

    rerearrange tags on homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,7 @@ import './NavBar.css'
 
 export const Home = () =>
     <div className="HomePage">
-      <img src="https://windermerehoodcanal.com/files/2018/03/content_content_Bookshelf_01.jpg" />
       <h1 className="appName">A Good Read</h1>
       <p className="description">A place to add, review, and comment on popular book choices.</p>
+      <img className="homeImg" src="https://windermerehoodcanal.com/files/2018/03/content_content_Bookshelf_01.jpg" />
     </div>
