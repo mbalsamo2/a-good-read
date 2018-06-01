874cec5cfb213285ddc31bceae35bac2e5814193
mbalsamo <github email address>
2018-06-01 11:40:26 -0400

    build out backend api create controller to handle when new book form is submitted

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,4 +12,20 @@ class BooksController < ApplicationController
     render :json => @book
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.valid?
+      @book.save
+      render :json => @book
+    else
+      render json:{message: "There was an error, please try again."}
+    end
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :author, :summary, :review, :rating, :image_url, comment_ids:[])
+  end
 end
