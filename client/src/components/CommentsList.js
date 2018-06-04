2cfc72dcb0717952cf2a082258ac6682abd192b9
mbalsamo <github email address>
2018-06-04 17:14:46 -0400

    submits comment successfully from commentForm to api but cannot render in state

---
+++ client/src/components/CommentsList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../components/NavBar.css';
+import Book from './book.js';
+
+class CommentsList extends Component {
+  debugger
+  render() {
+    return(
+      <div>
+        <h1>This is a comment</h1>
+      </div>
+    )
+  }
+}
+
+export default CommentsList;
