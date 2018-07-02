596413ab01902a9888bf43410da7aba61e0e02cd
mbalsamo <github email address>
2018-07-02 17:13:31 -0400

    implement most liked book at top of book list code

--- client/src/components/BooksList.js
+++ client/src/components/BooksList.js
@@ -6,10 +6,27 @@ const BooksList = ({ booksList }) => {
   if (booksList) {
     //let reverseBooks = booksList.reverse()
 
-    const books = booksList.map((book, index) => {
+    // function compare(a,b) {
+    //   if (a.attr < b.attr)
+    //      return -1;
+    //   if (a.attr > b.attr)
+    //     return 1;
+    //   return 0;
+    // }
+    //
+    // canvasObjects.sort(compare);
+
+    const sortedBooks = booksList.sort(function(a,b) {return (a.likes < b.likes) ? 1 : ((b.likes < a.likes) ? -1 : 0);} );
+
+    const books = sortedBooks.map((book, index) => {
       return (
       <Book key={index} book={book} /> )
     })
+
+    // const books = booksList.map((book, index) => {
+    //   return (
+    //   <Book key={index} book={book} /> )
+    // })
 
     return (
       <div className="BooksPage">
